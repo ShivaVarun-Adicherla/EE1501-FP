6fea57b8194e1f379adb28d74520c4e0b3c0f6dc
Sarthak Kumar Mishra <97846397+rorkela@users.noreply.github.com>
2025-04-21 15:39:55 +0530

    more test.added top mod inputs and outputs

--- top_module.v
+++ top_module.v
@@ -1,4 +1,14 @@
-module top_module();
-initial begin
-  $display("Hello World");
+module top_module (
+    input wire mode,
+    input wire up,
+    input wire down,
+    input wire select,
+    input wire reset,
+    output [5:0][3:0] hhmmss,
+    output [7:0][3:0] ddmmyyyy,
+    output AM_mode,
+    output PM_mode,
+    output timer_buzzer,
+    output alarm_buzzer
+);
 endmodule
